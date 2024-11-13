0cdd855329ea9cfb6da8a8f44c9e2dd3ea872b22
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-11-14 00:12:46 +0100

    Añadir Swagger para NestJS y añadir la configuración básica

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +15,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // Retorna bad request si hay propiedades en el objeto no requeridas
     }),
   );
+
+  // Documentación Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Gamelib RESTFul API')
+    .setDescription('Gamelib endpoints')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Server is running on port ${process.env.PORT}`);
