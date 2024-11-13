32dbb69f8ab7b6f7fa91551a1c30e1e4c855f4de
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-11-13 23:55:32 +0100

    Añadir class-validator y class-transformer para validar y transformar datos de usuario. Configurar ValidationPipe para validación global.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Main');
+
+  // Configuración global de pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remueve todo lo que no está incluido en los DTOs
+      forbidNonWhitelisted: true, // Retorna bad request si hay propiedades en el objeto no requeridas
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Server is running on port ${process.env.PORT}`);
