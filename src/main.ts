c49b03b6061e633780012028def443cb68016f9f
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-11-13 23:36:33 +0100

    añadir log para indicar la URL y puerto en el que se ejecuta el servidor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
+
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Server is running on port ${process.env.PORT}`);
 }
 bootstrap();
