674742bfe3e873f900a5982bbafff7c8e5bb13d2
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-11-15 21:25:13 +0100

    Añadir campos con decoradores a CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString, Min, MinLength } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(3)
+    username: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(3)
+    fullname?: string;
+}
