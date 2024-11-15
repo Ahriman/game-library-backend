e51cf4403ca70a86190f3ffc80b2c4367d381666
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-11-15 22:04:18 +0100

    Implementar método create en GamesService y controlar cuándo se añade un juego que ya existe en la base de datos

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,10 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
+import { Game } from './entities/game.entity';
 
 @Injectable()
 export class GamesService {
-  create(createGameDto: CreateGameDto) {
+
+  private readonly logger = new Logger('GamesService');
+
+  constructor(
+    @InjectRepository(Game)
+    private readonly gameRepository: Repository<Game>,
+  ) {}
+
+  async create(createGameDto: CreateGameDto) {
+
+    try {
+
+      const game = this.gameRepository.create(createGameDto);
+      await this.gameRepository.save( game );
+
+      return game;
+      
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+
+
     return 'This action adds a new game';
   }
 
@@ -23,4 +47,16 @@ export class GamesService {
   remove(id: number) {
     return `This action removes a #${id} game`;
   }
+
+  private handleDBExceptions(error: any) {
+
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    // console.log(error);
+    throw new InternalServerErrorException('Unexpected error, check server logs');
+  }
+
 }
