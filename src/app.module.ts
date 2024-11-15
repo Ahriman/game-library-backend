7ced4c0893b0192012b788949b119c9f3a51fb31
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-11-15 22:33:00 +0100

    Añadir módulo common

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { GamesModule } from './games/games.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -25,6 +26,8 @@ import { GamesModule } from './games/games.module';
     UsersModule,
 
     GamesModule,
+
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
