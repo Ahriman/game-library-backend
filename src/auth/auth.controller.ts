9c709ca41ed9e281fedfa6bf0cff7153d9aa0608
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-11-17 00:52:08 +0100

    Crear archivo de barril para los DTO de auth y así simplificar las importaciones

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { RegisterUserDto } from './dto/register-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
+import { RegisterUserDto, LoginUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
