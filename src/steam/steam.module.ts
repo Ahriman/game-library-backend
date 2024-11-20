b38765f1bdb00674c9d0b2a5940dc83f3283ed49
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2024-11-20 18:27:04 +0100

    Añadir import de ConfigModule al módulo de Steam para que arranque el servidor

--- src/steam/steam.module.ts
+++ src/steam/steam.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { SteamService } from './steam.service';
 import { SteamController } from './steam.controller';
 import { GamesModule } from 'src/games/games.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule,
+    GamesModule,
+  ],
   controllers: [SteamController],
   providers: [SteamService],
-  imports: [GamesModule],
 })
 export class SteamModule {}
